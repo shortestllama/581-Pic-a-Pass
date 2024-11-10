036a233e46c1799ec7133bba630efe31838576c9
Caden LeCluyse <lecluyse20@gmail.com>
2024-11-10 12:26:18 -0600

    Added a class for checking breaches

---
+++ src/Breach.py
@@ -0,0 +1,46 @@
+import hashlib
+import requests
+
+class APIChecker:
+    # Initialize the checker with the api url
+    def __init__(self):
+        self.url = "https://api.pwnedpasswords.com/range/"
+    
+    # The API requires the first 5 characters of a sha1 hash
+    def _get_sha1_hash(self, password: str) -> str:
+        return hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+
+    # Query the have i been pwned api
+    def _query_api(self, hash_prefix: str) -> str:
+        try:
+            # Perform a get request with the format: api_url/hash_prefix 
+            response = requests.get(f"{self.url}{hash_prefix}")
+            response.raise_for_status()
+            # Return the response
+            return response.text
+        except requests.RequestException as e:
+            raise ConnectionError(f"Failed to check password: {str(e)}")
+
+    # This will actually check the password
+    def check_password(self, password: str):
+        # Generate the hash, and then split it into prefix and suffix
+        password_hash = self._get_sha1_hash(password)
+        hash_prefix = password_hash[:5]
+        hash_suffix = password_hash[5:]
+
+        # Get the api response, if not throw the error it gives
+        # Can potentially deal with and catch the error here I'm just not sure what we're doing yet
+        try:
+            api_response = self._query_api(hash_prefix)
+        except ConnectionError as e:
+            raise e
+
+        # Check if the suffix exists in the response
+        for line in api_response.splitlines():
+            suffix, count = line.split(':')
+            if suffix == hash_suffix:
+                # Return the number of data breaches
+                return True, int(count)
+
+        # Return false iff the suffix isnt in the response
+        return False, 0

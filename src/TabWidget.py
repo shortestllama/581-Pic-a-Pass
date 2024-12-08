0c706aa534c15b7c73f98d443aae4763f168b91b
Caden LeCluyse <lecluyse20@gmail.com>
2024-12-08 13:10:58 -0600

    Tryna make the tabs look better

--- src/TabWidget.py
+++ src/TabWidget.py
@@ -2,40 +2,60 @@
 TabWidget Class
 Contains specification of the TabWidget class for the main window of the program.
 '''
-from PyQt5 import QtWidgets, QtCore #Needed imports
+from PyQt5 import QtWidgets, QtCore, QtGui #Needed imports
 class TabBar(QtWidgets.QTabBar): #used within the TabWidget class.  This is the tabs on the side
     def tabSizeHint(self, index): #used for formatting
         s = QtWidgets.QTabBar.tabSizeHint(self, index) #get the size of this tab
         s.transpose() #rotate the tab
-        s = QtCore.QSize(150, 289) #THIS IS THE REAL SIZE OF THE TABS
+        s = QtCore.QSize(150, 30) #THIS IS THE REAL SIZE OF THE TABS
         return s #return it
 
     #how do we change the colors of the tabs?
-    def paintEvent(self, event): #creates the tabs within the tab bar
-        painter = QtWidgets.QStylePainter(self) #Creates the tab bars on the screen
-        opt = QtWidgets.QStyleOptionTab() #Gives the tab bars their style
+    
+    def paintEvent(self, event):
+        painter = QtWidgets.QStylePainter(self)
+        opt = QtWidgets.QStyleOptionTab()
 
-        for i in range(self.count()): #for each tab bar (here we have 2)
-            self.initStyleOption(opt, i) #create style options
-            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt) #draw the tab
-            painter.save() #save the tab
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            
 
-            s = opt.rect.size() #resize to specs
-            s.transpose() #rotate the tab so it is sideways instead of vertical
-            r = QtCore.QRect(QtCore.QPoint(), s) #shaped as a rectangle
-            r.moveCenter(opt.rect.center()) #center text
-            opt.rect = r #rectangle object is saved
+            # Draw tab shape
+            painter.drawRoundedRect(opt.rect, 5, 5)
+            
+            # Save the current painter state
+            painter.save()
+            
+            # Handle text positioning and rotation
+            s = opt.rect.size()
+            s.transpose()
+            r = QtCore.QRect(QtCore.QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
 
-            c = self.tabRect(i).center() #center rectangle object
-            painter.translate(c) #move the tabs to align
-            painter.rotate(90) #rotate the window that the tab is inside
-            painter.translate(-c) #move tab back so now it is correct location and orientation
-            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt); #Label the tab
-            painter.restore() #finish execution
-
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            
+            # Draw the text
+            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt)
+            painter.restore()
 
 class TabWidget(QtWidgets.QTabWidget):
     def __init__(self, *args, **kwargs):
-        QtWidgets.QTabWidget.__init__(self, *args, **kwargs) #init this object
-        self.setTabBar(TabBar(self)) #create tab bar object and link it
-        self.setTabPosition(QtWidgets.QTabWidget.West) #set on left side of window
+        super().__init__(*args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QtWidgets.QTabWidget.West)
+        
+        # Style the tab widget container
+        self.setStyleSheet("""
+            QTabWidget::pane {
+                border: 1px solid #BDBDBD;
+                border-radius: 5px;
+                background: white;
+            }
+            QTabWidget::tab-bar {
+                left: 5px;
+            }
+        """)

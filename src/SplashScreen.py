d5fbc1c54e9832152ab7adbbae23ed1e1304382d
jDeBok <113641695+jDeBok@users.noreply.github.com>
2024-12-08 16:05:52 -0600

    Added comments

--- src/SplashScreen.py
+++ src/SplashScreen.py
@@ -1,3 +1,20 @@
+'''
+Artifact: Pic-a-Pass SplashScreen.py
+Description: Splash screen window for startup
+Author(s): Jesse DeBok
+Precondition(s): None
+Postcondition(s): Main Window is created
+Error(s): None
+Side effect(s): None
+Invariant(s): None
+Known fault(s): None
+
+#########################################################################################
+| Author       |  Date      | Revise Description                                        |
+#########################################################################################
+| Jesse DeBok  | 11/07/24   | Document created                                          |
+#########################################################################################
+'''
 from PyQt5.QtWidgets import QSplashScreen, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
@@ -5,11 +22,11 @@ from PyQt5.QtCore import Qt
 class SplashScreen(QSplashScreen):
     def __init__(self):
         super().__init__()
-        pixmap = QPixmap("Pic-a-Pass_logo.png")  # Replace with your image path
-        self.setPixmap(pixmap)
+        pixmap = QPixmap("Pic-a-Pass_logo.png")  #our logo image
+        self.setPixmap(pixmap) #set its creation
 
-        # Optional: Add text to the splash screen
+        # Text on the bottom
         label = QLabel("Loading...", self)
         label.setStyleSheet("color: white; font-size: 16px;")
-        label.setAlignment(Qt.AlignCenter)
+        label.setAlignment(Qt.AlignCenter) #location in bar
         label.setGeometry(0, pixmap.height() - 30, pixmap.width(), 30)
